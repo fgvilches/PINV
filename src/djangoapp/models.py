0bce6160d760c7218c77fc87780963af0411759c
Felipe González <dev@fgvilches.info>
2020-02-13 23:59:55 -0300

    txt'less
    Se agrega inplementacion txtless a clasificaciones maquinarias ubicaciones y sububicaciones

--- src/djangoapp/models.py
+++ src/djangoapp/models.py
@@ -10,48 +10,37 @@ class Clasificaciones(models.Model):
 
     def __str__(self):
         return self.Clasificacion
+#Samee
+class Maquinarias(models.Model):
+    Maquinaria = models.CharField(max_length = 30, blank=True)
+
+    def __str__(self):
+        return self.Maquinaria
+#Samee
+class Ubicaciones(models.Model):
+    Ubicacion = models.CharField(max_length = 30, blank=True)
+
+    def __str__(self):
+        return self.Ubicacion
+#Samee
+class SubUbicaciones(models.Model):
+    SubUbicacion = models.CharField(max_length = 30, blank=True)
+
+    def __str__(self):
+        return self.SubUbicacion
+
 #Se dfine clase producto para mejor estructura
 class Producto(models.Model):
-    Nombre = models.CharField(max_length=30, blank=True,)
-    Codigo_Propio = models.CharField(
-        max_length=30,
-        blank=True,
-    )
-    Codigo_de_barras = models.CharField(
-        max_length=30,
-        blank=True,
-    )
-    Factura_asociada = models.CharField(
-        max_length=30,
-        blank=True,
-    )
-    Maqs = choiceExtractor(readfile("maquinarias","txt"))
-    Maquinaria = MultiSelectField(
-        choices = Maqs,
-        max_choices=100,
-        max_length=100,
-        blank=True,
-    )
-    Clasificacion = models.ForeignKey(Clasificaciones, blank=True, null=True,on_delete=models.CASCADE)
+    Nombre = models.CharField(max_length=30, blank=True)
+    Codigo_Propio = models.CharField(max_length=30, blank=True)
+    Codigo_de_barras = models.CharField(max_length=30, blank=True)
+    Factura_asociada = models.CharField(max_length=30, blank=True)
+    Maquinaria = models.ForeignKey(Maquinarias, blank=True, null=True, on_delete=models.CASCADE)
+    Clasificacion = models.ForeignKey(Clasificaciones, blank=True, null=True, on_delete=models.CASCADE)
     Cantidad_de_producto = models.CharField(max_length=5)
-    Cantidad_de_subproducto = models.CharField(
-        max_length=5,
-        blank=True,
-    )
-    Locations = choiceExtractor(readfile("ubicaciones","txt"))
-    Ubicacion = models.CharField(
-        blank=True,
-        max_length = 8,
-        choices = Locations,
-        default = "NINGUNA",
-    )
-    Sublocations = choiceExtractor(readfile("sububicaciones","txt"))
-    SubUbicacion = models.CharField(
-        max_length = 7,
-        choices = Sublocations,
-        default = "NINGUNA",
-        blank=True,
-    )
+    Cantidad_de_subproducto = models.CharField(max_length=5, blank=True)
+    Ubicacion = models.ForeignKey(Ubicaciones, blank=True, null=True, on_delete=models.CASCADE)
+    SubUbicacion = models.ForeignKey(SubUbicaciones, blank=True, null=True, on_delete=models.CASCADE)
     fecha_de_compra = models.DateField("Fecha de Compra(MM/DD/AAAA)",auto_now_add=False, auto_now=False, blank=True, null=True)
     timestamp = models.DateField(auto_now_add=True, auto_now=False, blank=True)
     def __str__(self):
